8affa5624543f71b9714b5cbef7da5e5bdb3a922
Mancunian1878 <limatov.murad@yandex.ru>
2017-04-16 20:49:52 +0300

    Web app ToDos

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
-
 var main = function () {
-	"use strict";
-	var toDos = [
-		"Закончить писать эту книгу",
-		"Вывести Грейси на прогулку в парк",
-		"Ответить на электронные письма",
-		"Подготовиться к лекции в понедельник",
-		"Обновить несколько новых задач",
-		"Купить продукты"
-	];
-	$(".tabs a span").toArray().forEach(function (element) {
-		// создаем обработчик щелчков для этого элемента
-		$(element).on("click", function () {
-		var $element = $(element),
-		$content;
-		$(".tabs a span").removeClass("active");
-		$element.addClass("active");
-		$("main .content").empty();
-		if ($element.parent().is(":nth-child(1)")) {
-			$content = $("<ul>");
-			var i;
-			for (i = toDos.length-1; i>=0; i--) {
-				$content.append($("<li>").text(toDos[i]));
-			};
-			$("main .content").append($content);
-			console.log("Щелчок на первой вкладке!");
-		} else if ($element.parent().is(":nth-child(2)")) {
-		$content = $("<ul>");
-		toDos.forEach(function (todo) {
-			$content.append($("<li>").text(todo));
-		});
-		$("main .content").append($content);
-		} else if ($element.parent().is(":nth-child(3)")) {
-			console.log("Щелчок на третьей вкладке!");
-		}
-			return false;
-		});
-		$(".tabs a:first-child span").trigger("click");
-	});
+    var toDos = ["Get groceries",
+                 "Make up some new ToDos",
+                 "Prep for Monday's class",
+                 "Answer emails",
+                 "Take Gracie to the park",
+                 "Finish writing this book"];
+
+    $(".tabs a span").toArray().forEach(function (element) {
+        var $element = $(element);
+
+        // create a click handler for this element
+        $element.on("click", function () {
+            var $content,
+                $input,
+                $button,
+                i;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+                
+                $content = $("<ul>");
+                for (i = toDos.length-1; i >= 0; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+            } else if ($element.parent().is(":nth-child(2)")) {
+                
+                $content = $("<ul>");
+                toDos.forEach(function (todo) {
+                    $content.append($("<li>").text(todo));
+                });
+            } else if ($element.parent().is(":nth-child(3)")) {
+                // input a new to-do
+                $input = $("<input>"),
+                $button = $("<button>").text("+");
+
+                $button.on("click", function () {
+                    if ($input.val() !== "") {
+                        toDos.push($input.val());
+                        $input.val("");
+                    }
+                });
+
+                $content = $("<div>").append($input).append($button);
+            
+            }
+
+            $("main .content").append($content);
+
+            return false;
+        });
+    });
+
+    $(".tabs a:first-child span").trigger("click");
 };
+
 $(document).ready(main);
